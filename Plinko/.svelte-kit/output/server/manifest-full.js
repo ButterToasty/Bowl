7022eb7ee0021fcb299f709490f077aa718388c4
ButterToasty <106360708+ButterToasty@users.noreply.github.com>
2024-09-20 16:37:23 +0000

    fixPlinko

--- Plinko/.svelte-kit/output/server/manifest-full.js
+++ Plinko/.svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["android-chrome-192x192.png","android-chrome-512x512.png","apple-touch-icon.png","favicon-16x16.png","favicon-32x32.png","favicon.ico","site.webmanifest"]),
 	mimeTypes: {".png":"image/png",".webmanifest":"application/manifest+json"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.DhnJvIo9.js","app":"_app/immutable/entry/app.D4f4lxBI.js","imports":["_app/immutable/entry/start.DhnJvIo9.js","_app/immutable/chunks/entry.Bs65xEWi.js","_app/immutable/chunks/scheduler.CN4FEajw.js","_app/immutable/chunks/index.8qYHx-Kq.js","_app/immutable/entry/app.D4f4lxBI.js","_app/immutable/chunks/scheduler.CN4FEajw.js","_app/immutable/chunks/index.DJqeu6aJ.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.MJPcsxFQ.js","app":"_app/immutable/entry/app.B4PtnWhj.js","imports":["_app/immutable/entry/start.MJPcsxFQ.js","_app/immutable/chunks/entry.DHAmMizX.js","_app/immutable/chunks/scheduler.CN4FEajw.js","_app/immutable/chunks/index.8qYHx-Kq.js","_app/immutable/entry/app.B4PtnWhj.js","_app/immutable/chunks/scheduler.CN4FEajw.js","_app/immutable/chunks/index.DJqeu6aJ.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- Plinko/build/_app/immutable/entry/start.DhnJvIo9.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.Bs65xEWi.js";export{t as start};
