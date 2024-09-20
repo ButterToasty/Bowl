de1e20196b7b1c064d084ece64d03356635a5a5b
ButterToasty <106360708+ButterToasty@users.noreply.github.com>
2024-09-20 13:37:51 +0000

    plinko plinko plinko

--- plinko/src/lib/components/SettingsWindow/index.ts
+++
@@ -1,1 +0,0 @@
-export { default } from './SettingsWindow.svelte';
--- plinko/src/lib/stores/layout.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const isGameSettingsOpen = writable<boolean>(false);
-
-export const isLiveStatsOpen = writable<boolean>(false);
