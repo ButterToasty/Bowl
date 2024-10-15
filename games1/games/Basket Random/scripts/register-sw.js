9d612517238c13d6e960bbf588aee7343fd42b90
ButterToasty <106360708+ButterToasty@users.noreply.github.com>
2024-10-15 17:16:02 +0000

    fixing

--- games1/games/Basket Random/scripts/register-sw.js
+++ games1/games/Basket Random/scripts/register-sw.js
@@ -2,7 +2,7 @@
 window.C3_RegisterSW = async function () {
 	if (navigator.serviceWorker)
 		try {
-			const a = await navigator.serviceWorker.register('https://buttertoasty.github.io/games1/games/Basket%20Randomsw.js', { scope: './' });
+			const a = await navigator.serviceWorker.register('https://buttertoasty.github.io/Bowl/games1/games/Basket%20Randomsw.js', { scope: './' });
 			console.info('Registered service worker on ' + a.scope);
 		} catch (a) {
 			console.warn('Failed to register service worker: ', a);
